b150a5c88f61c0704efe69800eed36e376ace00b
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2022-09-18 15:58:46 -0500

    debugging service worker

--- client/src-sw.js
+++ client/src-sw.js
@@ -19,6 +19,19 @@ const pageCache = new CacheFirst({
   ],
 });
 
+const imageCache = new CacheFirst({
+  cacheName: 'my-image-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxEntries: 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
